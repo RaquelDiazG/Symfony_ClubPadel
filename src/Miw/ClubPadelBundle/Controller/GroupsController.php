7a09a24796c4b4fa344e478db11adbc9374d237b
Raquel Díaz González <raquel.diazgonz@alumnos.upm.es>
2015-12-08 13:38:44 +0100

    Creada api para Groups

--- src/Miw/ClubPadelBundle/Controller/GroupsController.php
+++ src/Miw/ClubPadelBundle/Controller/GroupsController.php
@@ -3,222 +3,63 @@
 namespace Miw\ClubPadelBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use FOS\RestBundle\Controller\FOSRestController;
 use Miw\ClubPadelBundle\Entity\Groups;
-use Miw\ClubPadelBundle\Form\GroupsType;
 
 /**
  * Groups controller.
  *
  */
-class GroupsController extends Controller
-{
+class GroupsController extends FOSRestController {
 
     /**
      * Lists all Groups entities.
-     *
      */
-    public function indexAction()
-    {
+    public function getAllAction() {
         $em = $this->getDoctrine()->getManager();
-
         $entities = $em->getRepository('MiwClubPadelBundle:Groups')->findAll();
-
-        return $this->render('MiwClubPadelBundle:Groups:index.html.twig', array(
-            'entities' => $entities,
-        ));
+        return $entities;
     }
+
     /**
      * Creates a new Groups entity.
-     *
      */
-    public function createAction(Request $request)
-    {
+    public function createAction(Request $request) {
         $entity = new Groups();
-        $form = $this->createCreateForm($entity);
-        $form->handleRequest($request);
-
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($entity);
-            $em->flush();
-
-            return $this->redirect($this->generateUrl('group_show', array('id' => $entity->getId())));
-        }
-
-        return $this->render('MiwClubPadelBundle:Groups:new.html.twig', array(
-            'entity' => $entity,
-            'form'   => $form->createView(),
-        ));
-    }
-
-    /**
-     * Creates a form to create a Groups entity.
-     *
-     * @param Groups $entity The entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createCreateForm(Groups $entity)
-    {
-        $form = $this->createForm(new GroupsType(), $entity, array(
-            'action' => $this->generateUrl('group_create'),
-            'method' => 'POST',
-        ));
-
-        $form->add('submit', 'submit', array('label' => 'Create'));
-
-        return $form;
-    }
-
-    /**
-     * Displays a form to create a new Groups entity.
-     *
-     */
-    public function newAction()
-    {
-        $entity = new Groups();
-        $form   = $this->createCreateForm($entity);
-
-        return $this->render('MiwClubPadelBundle:Groups:new.html.twig', array(
-            'entity' => $entity,
-            'form'   => $form->createView(),
-        ));
+        $entity->setName($request->get("name"));
+        $entity->setRoles($request->get("roles"));
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($entity);
+        $em->flush();
+        return $entity;
     }
 
     /**
      * Finds and displays a Groups entity.
-     *
      */
-    public function showAction($id)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $entity = $em->getRepository('MiwClubPadelBundle:Groups')->find($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Groups entity.');
-        }
-
-        $deleteForm = $this->createDeleteForm($id);
-
-        return $this->render('MiwClubPadelBundle:Groups:show.html.twig', array(
-            'entity'      => $entity,
-            'delete_form' => $deleteForm->createView(),
-        ));
+    public function getAction(Groups $group) {
+        return $group;
     }
 
-    /**
-     * Displays a form to edit an existing Groups entity.
-     *
-     */
-    public function editAction($id)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $entity = $em->getRepository('MiwClubPadelBundle:Groups')->find($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Groups entity.');
-        }
-
-        $editForm = $this->createEditForm($entity);
-        $deleteForm = $this->createDeleteForm($id);
-
-        return $this->render('MiwClubPadelBundle:Groups:edit.html.twig', array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        ));
-    }
-
-    /**
-    * Creates a form to edit a Groups entity.
-    *
-    * @param Groups $entity The entity
-    *
-    * @return \Symfony\Component\Form\Form The form
-    */
-    private function createEditForm(Groups $entity)
-    {
-        $form = $this->createForm(new GroupsType(), $entity, array(
-            'action' => $this->generateUrl('group_update', array('id' => $entity->getId())),
-            'method' => 'PUT',
-        ));
-
-        $form->add('submit', 'submit', array('label' => 'Update'));
-
-        return $form;
-    }
     /**
      * Edits an existing Groups entity.
-     *
      */
-    public function updateAction(Request $request, $id)
-    {
+    public function updateAction(Request $request, Groups $group) {
         $em = $this->getDoctrine()->getManager();
-
-        $entity = $em->getRepository('MiwClubPadelBundle:Groups')->find($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Groups entity.');
-        }
-
-        $deleteForm = $this->createDeleteForm($id);
-        $editForm = $this->createEditForm($entity);
-        $editForm->handleRequest($request);
-
-        if ($editForm->isValid()) {
-            $em->flush();
-
-            return $this->redirect($this->generateUrl('group_edit', array('id' => $id)));
-        }
-
-        return $this->render('MiwClubPadelBundle:Groups:edit.html.twig', array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        ));
+        $em->getRepository('MiwClubPadelBundle:Groups');
+        $group->setName($request->get("name"));
+        $group->setRoles($request->get("roles"));
+        $em->flush();
+        return $group;
     }
+
     /**
      * Deletes a Groups entity.
-     *
      */
-    public function deleteAction(Request $request, $id)
-    {
-        $form = $this->createDeleteForm($id);
-        $form->handleRequest($request);
-
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $entity = $em->getRepository('MiwClubPadelBundle:Groups')->find($id);
-
-            if (!$entity) {
-                throw $this->createNotFoundException('Unable to find Groups entity.');
-            }
-
-            $em->remove($entity);
-            $em->flush();
-        }
-
-        return $this->redirect($this->generateUrl('group'));
+    public function deleteAction(Request $request, Groups $group) {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($group);
+        $em->flush();
     }
 
-    /**
-     * Creates a form to delete a Groups entity by id.
-     *
-     * @param mixed $id The entity id
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm($id)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('group_delete', array('id' => $id)))
-            ->setMethod('DELETE')
-            ->add('submit', 'submit', array('label' => 'Delete'))
-            ->getForm()
-        ;
-    }
 }
